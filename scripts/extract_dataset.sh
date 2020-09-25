dbdea9cf16fd07e89d3ffdeb5ea9212ebdb9dfbf
Hyungon_Ryu <yhgon@users.noreply.github.com>
2020-09-25 21:39:22 +0900

    Create extract_dataset.sh

---
+++ scripts/extract_dataset.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+DATA_DIR="LJSpeech-1.1"
+
+LJS_ARCH="LJSpeech-1.1.tar.bz2"
+LJS_URL="http://data.keithito.com/data/speech/${LJS_ARCH}"
+
+if [ ! -d ${DATA_DIR} ]; then
+  echo "Extracting ${LJS_ARCH} ..."
+  tar jxvf ${LJS_ARCH}
+  rm -f ${LJS_ARCH}
+fi
